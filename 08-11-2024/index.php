e9ffbe5257a8d7880e93f38c032e1ad93e60f57a
Lenicio Jr <lenicio.junior@hotmail.com>
2024-11-08 17:31:24 -0300

    Commit

---
+++ 08-11-2024/index.php
@@ -0,0 +1,682 @@
+<?php
+$livros = [
+  [
+    "isbn" => uniqid(),
+    "titulo" => "É Assim que Acaba",
+    "genero" => "Romance",
+    "autor" => "Colleen Hoover",
+    "anoLancamento" => 2016,
+    "status" => false
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "1984",
+    "genero" => "Distopia",
+    "autor" => "George Orwell",
+    "anoLancamento" => 1949,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Dom Casmurro",
+    "genero" => "Romance",
+    "autor" => "Machado de Assis",
+    "anoLancamento" => 1899,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Alquimista",
+    "genero" => "Ficção",
+    "autor" => "Paulo Coelho",
+    "anoLancamento" => 1988,
+    "status" => false
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Pequeno Príncipe",
+    "genero" => "Fábula",
+    "autor" => "Antoine de Saint-Exupéry",
+    "anoLancamento" => 1943,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "A Menina que Roubava Livros",
+    "genero" => "Drama",
+    "autor" => "Markus Zusak",
+    "anoLancamento" => 2005,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Harry Potter e a Pedra Filosofal",
+    "genero" => "Fantasia",
+    "autor" => "J.K. Rowling",
+    "anoLancamento" => 1997,
+    "status" => false
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Senhor dos Anéis: A Sociedade do Anel",
+    "genero" => "Fantasia",
+    "autor" => "J.R.R. Tolkien",
+    "anoLancamento" => 1954,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Vidas Secas",
+    "genero" => "Ficção",
+    "autor" => "Graciliano Ramos",
+    "anoLancamento" => 1938,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "A Hora da Estrela",
+    "genero" => "Ficção",
+    "autor" => "Clarice Lispector",
+    "anoLancamento" => 1977,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Capitães da Areia",
+    "genero" => "Romance",
+    "autor" => "Jorge Amado",
+    "anoLancamento" => 1937,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Memórias Póstumas de Brás Cubas",
+    "genero" => "Romance",
+    "autor" => "Machado de Assis",
+    "anoLancamento" => 1881,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Cortiço",
+    "genero" => "Romance",
+    "autor" => "Aluísio Azevedo",
+    "anoLancamento" => 1890,
+    "status" => false
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "A Revolução dos Bichos",
+    "genero" => "Fábula",
+    "autor" => "George Orwell",
+    "anoLancamento" => 1945,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Morro dos Ventos Uivantes",
+    "genero" => "Romance",
+    "autor" => "Emily Brontë",
+    "anoLancamento" => 1847,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Orgulho e Preconceito",
+    "genero" => "Romance",
+    "autor" => "Jane Austen",
+    "anoLancamento" => 1813,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Cem Anos de Solidão",
+    "genero" => "Realismo Mágico",
+    "autor" => "Gabriel García Márquez",
+    "anoLancamento" => 1967,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Nome da Rosa",
+    "genero" => "Romance Histórico",
+    "autor" => "Umberto Eco",
+    "anoLancamento" => 1980,
+    "status" => false
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "As Crônicas de Nárnia: O Leão, a Feiticeira e o Guarda-Roupa",
+    "genero" => "Fantasia",
+    "autor" => "C.S. Lewis",
+    "anoLancamento" => 1950,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "A Metamorfose",
+    "genero" => "Ficção",
+    "autor" => "Franz Kafka",
+    "anoLancamento" => 1915,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Crime e Castigo",
+    "genero" => "Romance",
+    "autor" => "Fiódor Dostoiévski",
+    "anoLancamento" => 1866,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Idiota",
+    "genero" => "Romance",
+    "autor" => "Fiódor Dostoiévski",
+    "anoLancamento" => 1869,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Apanhador no Campo de Centeio",
+    "genero" => "Romance",
+    "autor" => "J.D. Salinger",
+    "anoLancamento" => 1951,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Velho e o Mar",
+    "genero" => "Ficção",
+    "autor" => "Ernest Hemingway",
+    "anoLancamento" => 1952,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Moby Dick",
+    "genero" => "Aventura",
+    "autor" => "Herman Melville",
+    "anoLancamento" => 1851,
+    "status" => false
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Conde de Monte Cristo",
+    "genero" => "Aventura",
+    "autor" => "Alexandre Dumas",
+    "anoLancamento" => 1844,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Os Miseráveis",
+    "genero" => "Romance",
+    "autor" => "Victor Hugo",
+    "anoLancamento" => 1862,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Retrato de Dorian Gray",
+    "genero" => "Ficção",
+    "autor" => "Oscar Wilde",
+    "anoLancamento" => 1890,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Drácula",
+    "genero" => "Terror",
+    "autor" => "Bram Stoker",
+    "anoLancamento" => 1897,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Frankenstein",
+    "genero" => "Terror",
+    "autor" => "Mary Shelley",
+    "anoLancamento" => 1818,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Sol é para Todos",
+    "genero" => "Drama",
+    "autor" => "Harper Lee",
+    "anoLancamento" => 1960,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Código Da Vinci",
+    "genero" => "Suspense",
+    "autor" => "Dan Brown",
+    "anoLancamento" => 2003,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "As Aventuras de Sherlock Holmes",
+    "genero" => "Mistério",
+    "autor" => "Arthur Conan Doyle",
+    "anoLancamento" => 1892,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Jogos Vorazes",
+    "genero" => "Distopia",
+    "autor" => "Suzanne Collins",
+    "anoLancamento" => 2008,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Percy Jackson e o Ladrão de Raios",
+    "genero" => "Fantasia",
+    "autor" => "Rick Riordan",
+    "anoLancamento" => 2005,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Hobbit",
+    "genero" => "Fantasia",
+    "autor" => "J.R.R. Tolkien",
+    "anoLancamento" => 1937,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "A Culpa é das Estrelas",
+    "genero" => "Romance",
+    "autor" => "John Green",
+    "anoLancamento" => 2012,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Diário de Anne Frank",
+    "genero" => "Biografia",
+    "autor" => "Anne Frank",
+    "anoLancamento" => 1947,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "A Divina Comédia",
+    "genero" => "Poesia",
+    "autor" => "Dante Alighieri",
+    "anoLancamento" => 1320,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Ilíada",
+    "genero" => "Épico",
+    "autor" => "Homero",
+    "anoLancamento" => -750,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Odisseia",
+    "genero" => "Épico",
+    "autor" => "Homero",
+    "anoLancamento" => -700,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Hamlet",
+    "genero" => "Tragédia",
+    "autor" => "William Shakespeare",
+    "anoLancamento" => 1603,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Macbeth",
+    "genero" => "Tragédia",
+    "autor" => "William Shakespeare",
+    "anoLancamento" => 1606,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Grande Gatsby",
+    "genero" => "Romance",
+    "autor" => "F. Scott Fitzgerald",
+    "anoLancamento" => 1925,
+    "status" => false
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Lolita",
+    "genero" => "Romance",
+    "autor" => "Vladimir Nabokov",
+    "anoLancamento" => 1955,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "A Montanha Mágica",
+    "genero" => "Romance",
+    "autor" => "Thomas Mann",
+    "anoLancamento" => 1924,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Processo",
+    "genero" => "Ficção",
+    "autor" => "Franz Kafka",
+    "anoLancamento" => 1925,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Estrangeiro",
+    "genero" => "Ficção",
+    "autor" => "Albert Camus",
+    "anoLancamento" => 1942,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "A Náusea",
+    "genero" => "Filosofia",
+    "autor" => "Jean-Paul Sartre",
+    "anoLancamento" => 1938,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Ensaio sobre a Cegueira",
+    "genero" => "Ficção",
+    "autor" => "José Saramago",
+    "anoLancamento" => 1995,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Memorial do Convento",
+    "genero" => "Romance",
+    "autor" => "José Saramago",
+    "anoLancamento" => 1982,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "A Insustentável Leveza do Ser",
+    "genero" => "Romance",
+    "autor" => "Milan Kundera",
+    "anoLancamento" => 1984,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Mundo de Sofia",
+    "genero" => "Filosofia",
+    "autor" => "Jostein Gaarder",
+    "anoLancamento" => 1991,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Os Irmãos Karamázov",
+    "genero" => "Romance",
+    "autor" => "Fiódor Dostoiévski",
+    "anoLancamento" => 1880,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Guerra e Paz",
+    "genero" => "Romance",
+    "autor" => "Liev Tolstói",
+    "anoLancamento" => 1869,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Amor nos Tempos do Cólera",
+    "genero" => "Romance",
+    "autor" => "Gabriel García Márquez",
+    "anoLancamento" => 1985,
+    "status" => false
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Cem Sonetos de Amor",
+    "genero" => "Poesia",
+    "autor" => "Pablo Neruda",
+    "anoLancamento" => 1959,
+    "status" => false
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Caçador de Pipas",
+    "genero" => "Drama",
+    "autor" => "Khaled Hosseini",
+    "anoLancamento" => 2003,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Mil Soles Esplêndidos",
+    "genero" => "Drama",
+    "autor" => "Khaled Hosseini",
+    "anoLancamento" => 2007,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Símbolo Perdido",
+    "genero" => "Suspense",
+    "autor" => "Dan Brown",
+    "anoLancamento" => 2009,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Lobo da Estepe",
+    "genero" => "Ficção",
+    "autor" => "Hermann Hesse",
+    "anoLancamento" => 1927,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Sidarta",
+    "genero" => "Filosofia",
+    "autor" => "Hermann Hesse",
+    "anoLancamento" => 1922,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "A Cidade e as Serras",
+    "genero" => "Romance",
+    "autor" => "Eça de Queirós",
+    "anoLancamento" => 1901,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Primo Basílio",
+    "genero" => "Romance",
+    "autor" => "Eça de Queirós",
+    "anoLancamento" => 1878,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Vermelho e o Negro",
+    "genero" => "Romance",
+    "autor" => "Stendhal",
+    "anoLancamento" => 1830,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Madame Bovary",
+    "genero" => "Romance",
+    "autor" => "Gustave Flaubert",
+    "anoLancamento" => 1856,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Cândido",
+    "genero" => "Sátira",
+    "autor" => "Voltaire",
+    "anoLancamento" => 1759,
+    "status" => false
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Mestre e Margarida",
+    "genero" => "Ficção",
+    "autor" => "Mikhail Bulgakov",
+    "anoLancamento" => 1967,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "O Alienista",
+    "genero" => "Conto",
+    "autor" => "Machado de Assis",
+    "anoLancamento" => 1882,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Grande Sertão: Veredas",
+    "genero" => "Romance",
+    "autor" => "João Guimarães Rosa",
+    "anoLancamento" => 1956,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Iracema",
+    "genero" => "Romance",
+    "autor" => "José de Alencar",
+    "anoLancamento" => 1865,
+    "status" => true
+  ],
+  [
+    "isbn" => uniqid(),
+    "titulo" => "Memórias de um Sargento de Milícias",
+    "genero" => "Romance",
+    "autor" => "Manuel Antônio de Almeida",
+    "anoLancamento" => 1854,
+    "status" => true
+  ],
+];
+
+# Verifica se o formulário foi enviado, e se recebeu corretamente as informações
+if (!empty($_GET["filtro-genero"])) {
+  $genero = $_GET["filtro-genero"];
+
+  # Percorre a lista de livros
+  foreach ($livros as $indice => $livro) {
+    if (!( trim(strtolower($livro["genero"])) ==  trim(strtolower($genero)))) {
+      unset($livros[$indice]); # Remove o livro da lista, caso genero seja o pesquisado
+    }
+  }
+}
+
+if (!empty($_GET["filtro-ano"])) {
+  $ano = $_GET["filtro-ano"];
+
+  # Percorre a lista de livros
+  foreach ($livros as $indice => $livro) {
+    if (!($livro["anoLancamento"] == $ano)) {
+      unset($livros[$indice]); # Remove o livro da lista, caso genero seja o pesquisado
+    }
+  }
+}
+
+if (!empty($_GET["filtro-status"])) {
+  $filtroStatus = $_GET["filtro-status"];
+
+  if ($filtroStatus == "disponivel") {
+    foreach ($livros as $indice => $livro) {
+      if (!($livro["status"] == $filtroStatus)) {
+        unset($livros[$indice]); 
+      }
+    }
+  }
+
+}  
+
+if (!empty($_GET["filtro-status"])) {
+  $filtroStatus = $_GET["filtro-status"];
+
+  if ($filtroStatus == "indisponivel") {
+    foreach ($livros as $indice => $livro) {
+      if ($livro["status"] == $filtroStatus) {
+        unset($livros[$indice]); 
+      }
+    }
+  }
+
+}  
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <form action="./index.php" method="get">
+    <label for="filtro-genero">Genero: </label>
+    <input type="text" name="filtro-genero" id="filtro-genero">
+
+    <label for="filtro-ano">Ano de Lançamento: </label>
+    <input type="number" name="filtro-ano" id="filtro-ano">
+
+    <select name="filtro-status">
+      <option></option>
+      <option value="disponivel">Disponível</option>
+      <option value="indisponivel">Indisponível</option>
+    </select>
+
+    <button type="submit">Pesquisar</button>
+  </form>
+
+  <table border="1">
+    <thead>
+      <tr>
+        <th>Titulo</th>
+        <th>Genero</th>
+        <th>Autor</th>
+        <th>Ano de Lançamento</th>
+        <th>Disponivel</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($livros as $livro): ?>
+        <tr>
+          <td><?= $livro["titulo"] ?></td>
+          <td><?= $livro["genero"] ?></td>
+          <td><?= $livro["autor"] ?></td>
+          <td><?= $livro["anoLancamento"] ?></td>
+          <td><?= $livro["status"] ? "Disponível" : "Indisponível" ?></td>
+        </tr>
+      <?php endforeach ?>
+    </tbody>
+  </table>
+</body>
+
+</html>
